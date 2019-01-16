2c15fd561431827e9dfd9ec7a992bf0748483017
Qianli Ma <mqlzdfz@gmail.com>
2019-01-15 20:35:12 -0500

    update directory

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install Powerline Fonts
-pwd='pwd'
+dir=$(pwd)
 cd ~ && git clone https://github.com/powerline/fonts.git --depth=1
 cd ~/fonts && ./install.sh
 rm rf ~/fonts
@@ -11,9 +11,9 @@ case "$OSTYPE" in
         echo "Running on OSX"
         # MACOS system
         # Setup oh-my-zsh
-        $pwd/macos/oh-my-zsh.sh
+        $dir/macos/oh-my-zsh.sh
         # Install homebrew
-        $pwd/macos/brew.sh
+        $dir/macos/brew.sh
         cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
         exit 0
     ;;
