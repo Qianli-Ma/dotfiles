a6fde8100ec6e41f718909d689111470b003c41e
Qianli Ma <mqlzdfz@gmail.com>
2019-01-15 20:28:00 -0500

    update autosetup

--- setup.sh
+++ setup.sh
@@ -4,12 +4,20 @@ cd ~ && git clone https://github.com/powerline/fonts.git --depth=1
 cd ~/fonts && ./install.sh
 rm ~/fonts
 # Autosetup script
-if [["$OSTYPE" == "darwin"*]]; then
-    # MACOS system
-    # Install homebrew
-    ./macos/brew.sh
-    # Setup oh-my-zsh
-    ./macos/oh-my-zsh.sh
-    cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
-    
-fi
+case "$OSTYPE" in
+    solaris*) echo "SOLARIS" ;;
+    darwin*)
+        echo "Running on OSX"
+        # MACOS system
+        # Install homebrew
+        ./macos/brew.sh
+        # Setup oh-my-zsh
+        ./macos/oh-my-zsh.sh
+        cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+        exit 0
+    ;;
+    linux*)   echo "LINUX" ;;
+    bsd*)     echo "BSD" ;;
+    msys*)    echo "WINDOWS" ;;
+    *)        echo "unknown: $OSTYPE" ;;
+esac
